cc21afe45ba081cef70eb5fa15a4b0ab0fa01c31
Luis Delgado <ledodo99@hotmial.com>
2018-03-23 11:07:35 -0600

    Creacion de clases

---
+++ HDT8/src/hdt8/Node.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hdt8;
+
+/**
+ *
+ * @author ledod
+ */
+public class Node<E> {
+    private Paciente value;
+    protected  Node<E> left;
+    protected  Node<E> right;        
+    
+    public Node()
+    {
+        value=null;
+        left=null;
+        right=null;
+        
+                
+    }
+
+    public Node (Paciente value) {
+        this.value = value;
+        left = null;
+        right = null;
+    }
+    
+}
---
+++ HDT8/src/hdt8/Paciente.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hdt8;
+
+/**
+ *
+ * @author ledod
+ */
+public class Paciente {
+    private String nombre;
+    private String padecimiento;
+    private String prioridad;
+
+    public Paciente(String nombre, String padecimiento, String prioridad) {
+        this.nombre = nombre;
+        this.padecimiento = padecimiento;
+        this.prioridad = prioridad;
+    }
+    
+    public Paciente(){
+        nombre=null;
+        padecimiento=null;
+        prioridad=null;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPadecimiento() {
+        return padecimiento;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    
+    
+}
